7c527d4f03a7f907a33fe476dc197f13be28ac95
Cecilia Condarco <ana.condarco@ucb.edu.bo>
2025-12-03 17:56:06 -0400

    Add files via upload

---
+++ estructuras.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Fecha
+{
+    int mes;
+    int dia;
+    int anio;
+};
+
+struct DatosCliente
+{
+    char CI_Cliente[30];
+    char nombre[30];
+    char apellido[30];
+    Fecha fechaNacimiento;
+    int telefono;
+    char correo[30];
+};
+
+struct DatosEmpleado
+{
+    char CI_Empleado[10];
+    char nombre[30];
+    char apellido[30];
+    int rango;
+};
+
+struct DatosProducto
+{
+    int codigo;
+    char modelo[30];
+    int categoria;
+    float precioVenta;
+    int stock;
+    bool eliminado;
+};
+
+struct ventaProducto
+{
+    int numeroFactura;
+    int codigoProducto;
+    char CI_Cliente[10];
+    char CI_Empleado[10];
+    Fecha fechaDeVenta;
+    int cantidad;
+    float precioUnitario;
+};
+
+struct Reparaciones
+{
+    char CI_cliente[10];
+    int codigoProducto;
+    int numeroFactura;
+    char descripcion[100];
+    float precioReparacion;
+    Fecha fechaReparacion;
+};
+
+struct datosFactura
+{
+    int numeroFactura
+    char CI_cliente[10];
+    Fecha fecha_emision_factura;
+    int metodo_pago;
+};
+
+
+
+int main()
+{
+
+    return 0;
+}
