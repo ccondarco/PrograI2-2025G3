4e4f0eed36031581724d560950f0f3e116d44ee5
Cecilia Condarco <ana.condarco@ucb.edu.bo>
2025-12-17 18:43:55 -0400

    Update ProgramaPrincipal.cpp

--- ProgramaPrincipal.cpp
+++ ProgramaPrincipal.cpp
@@ -5,28 +5,6 @@
 
 using namespace std;
 
-void crear_productos_prueba(string nombre_archivo_productos);
-void menu_facturacion(string nombre_archivo_ventas, string nombre_archivo_facturas, vector<string> metodos_pago, string nombre_archivo_productos);
-void adicionar_venta(string nombre_archivo_ventas);
-void modificar_venta(string nombre_archivo_ventas);
-void eliminar_venta(string nombre_archivo_ventas);
-int generar_numero_factura(string nombre_archivo_facturas);
-void crear_factura(string nombre_archivo_facturas, string nombre_archivo_ventas, vector<string> metodos_pago, string nombre_archivo_productos);
-void anular_factura(string nombre_archivo_facturas, string nombre_archivo_ventas, string nombre_archivo_productos);
-void listar_ventas_pendientes(string nombre_archivo_ventas);
-void listar_facturas(string nombre_archivo_facturas, vector<string> metodos_pago, string nombre_archivo_ventas);
-void mostrar_detalle_factura(string nombre_archivo_facturas, string nombre_archivo_ventas);
-void menu_mostrar_detalle_factura(string nombre_archivo_ventas, string nombre_archivo_facturas);
-void descontar_stock(int codigoProducto, int cantidad_vendida, string nombre_archivo_productos);
-void sumar_stock(int codigoProducto, int cantidad_devuelta, string nombre_archivo_productos);
-void reporte_ventas_cliente(string nombre_archivo_ventas);
-void reporte_ventas_diarias(string nombre_archivo_ventas);
-void reporte_diario_productos_disponibles(string nombre_archivo_productos);
-void alerta_stock_producto(int codigoProducto, string nombre_archivo_productos, int limite);
-void menu_reportes(string nombre_archivo_ventas, string nombre_archivo_productos);
-void reporte_temporada_alta(string nombre_archivo_ventas);
-void menu_principal(string nombre_archivo_ventas, string nombre_archivo_facturas, string nombre_archivo_productos, vector<string> metodos_pago);
-
 int main()
 {
     system("chcp 65001");
@@ -43,3 +21,4 @@ int main()
 
     return 0;
 }
+
